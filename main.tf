5f447496b7e0dabc6e8404b177d9e394676be719
AKSHAY ASHOK CHANDANSHIVE <49795892+chandanshive@users.noreply.github.com>
2021-09-01 12:06:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_security_group" "hello-terra-ssh-http" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.1.0.0/24"]
   }
 
   ingress {
